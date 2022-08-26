bb654272a98ffd86e89469b96d9556764d93203e
emreken <emre.eken.59@gmail.com>
2022-08-26 13:43:12 +0300

    search box design

--- app/src/main/java/com/ekenapp/weatherapp/MainActivity.kt
+++ app/src/main/java/com/ekenapp/weatherapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ekenapp.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.ekenapp.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
